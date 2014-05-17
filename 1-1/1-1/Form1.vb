70c2adbe18a2c6c12e07be9e0271c9e6c54db624
jiangxincode <jinagxinnju@163.com>
2014-05-17 10:18:46 +0800

    Version 0.01
    Initial code

---
+++ 1-1/1-1/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+        Call MyMove()
+    End Sub
+    Sub MyMove()
+        Label1.Top = Label1.Top + 5
+        If Label1.Top > Me.Height Then Label1.Top = 0
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Enabled = False
+        Call MyMove()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Call MyMove()
+    End Sub
+End Class
---
+++ 2-1/2-1/窗体.vb
@@ -0,0 +1,10 @@
+﻿Public Class 窗体
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ForeColor = Color.Red
+    End Sub
+
+    Private Sub 窗体_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
